29618c6bc59a76fe27cebed940fe42eb62b07f18
NgocNguyen <ducngocbk@gmail.com>
2024-01-28 21:03:47 +0700

    Add server

---
+++ server-ssr/src/main/java/org/com/ssr/ServerSideRenderApplication.java
@@ -0,0 +1,13 @@
+package org.com.ssr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideRenderApplication.class, args);
+	}
+
+}
---
+++ server-ssr/src/test/java/org/com/ssr/ServerSideRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.ssr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
